0d4867276894a88b464fa5a9cde684cfd9290ef2
Joey <786678257@qq.com>
2022-06-15 18:01:52 +0800

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+   <router-view/>
+</template>
+
+<script lang="ts" setup>
+ document.documentElement.classList.add('dark')
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/home/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+  </div>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style scoped>
+</style>
